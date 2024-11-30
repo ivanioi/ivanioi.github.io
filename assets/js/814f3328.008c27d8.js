880aaf1fc0077c944e4e5f9acb8e1271b42941d2
ivan zhang <ivanohohoh@gmail.com>
2024-12-01 05:27:38 +0800

    'little change'

--- assets/js/814f3328.008c27d8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome","unlisted":false,"date":"2024-11-27T00:00:00.000Z"}]}')}}]);
